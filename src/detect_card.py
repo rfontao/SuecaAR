29bc1c889ed447c37b489340fa07ff58f0fc7625
rfontao <ricardo.fontao@gmail.com>
2022-10-06 01:26:28 +0100

    Experiments with template matching

--- src/detect_card.py
+++ src/detect_card.py
@@ -12,6 +12,8 @@ if (len(sys.argv) < 2):
 camera = Camera(0, sys.argv[1])
 detector = CardDetector(True)
 
+template = cv.imread("../cards/simple/4d.jpg", cv.IMREAD_COLOR)
+
 while True:
     frame = camera.get_frame()
     cards = detector.detect(frame.copy())
@@ -26,5 +28,23 @@ while True:
         M = cv.getPerspectiveTransform(cards[0], dst)
         warp = cv.warpPerspective(frame, M, (200, 400))
         cv.imshow("Warped", warp)
+
+        method = cv.TM_CCOEFF_NORMED
+        result = cv.matchTemplate(warp, template, method)
+        cv.normalize(result, result, 0, 1, cv.NORM_MINMAX, -1)
+        _minVal, _maxVal, minLoc, maxLoc = cv.minMaxLoc(result, None)
+        if (method == cv.TM_SQDIFF or method == cv.TM_SQDIFF_NORMED):
+            matchLoc = minLoc
+        else:
+            matchLoc = maxLoc
+
+        cv.rectangle(frame, matchLoc, (matchLoc[0] + template.shape[0],
+                     matchLoc[1] + template.shape[1]), (0, 0, 0), 2, 8, 0)
+        cv.rectangle(result, matchLoc, (matchLoc[0] + template.shape[0],
+                     matchLoc[1] + template.shape[1]), (0, 0, 0), 2, 8, 0)
+
+                    
+        cv.imshow("Original", frame)
+        cv.imshow("Matching", result)
 
     cv.waitKey(50)
