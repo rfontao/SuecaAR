75e0dea725320fd5eb6da34d21ebdf5627052f95
Henrique Ribeiro <gamingwithapotato@gmail.com>
2022-10-08 18:26:51 +0100

    Done with innitial template matching, needs to resize images in order to get better results

--- src/detect_card.py
+++ src/detect_card.py
@@ -1,8 +1,10 @@
+from copyreg import constructor
 import cv2 as cv
 import numpy as np
 import sys
 from camera import Camera
 from card_detection import CardDetector
+import imutils # used to resize image
 
 if (len(sys.argv) < 2):
     print(f"Usage: {sys.argv[0]} <file_to_read(.npz format)>")
@@ -12,7 +14,13 @@ if (len(sys.argv) < 2):
 camera = Camera(sys.argv[2], sys.argv[1])
 detector = CardDetector(True)
 
-template = cv.imread("../cards/simple/4d.jpg", cv.IMREAD_COLOR)
+template = cv.imread("../cards/full/1.png", cv.IMREAD_COLOR)
+template = template[0:200, 0:100]
+templateVal = template[5:90, 10:900] # gets the value of the card
+templateType = template[90:200, 10:90] # gets the type of the card
+
+cv.imshow("TemplateVal", templateVal)
+cv.imshow("TemplateType", templateType)
 
 while True:
     frame = camera.get_frame()
@@ -27,24 +35,49 @@ while True:
 
         M = cv.getPerspectiveTransform(detector.sortCardPoints(cards[0]), dst)
         warp = cv.warpPerspective(frame, M, (200, 400))
-        cv.imshow("Warped", warp)
+
+        # cv.imshow("Warped", warp)
+        cropped = warp[0:int(warp.shape[0]/4), 0:int(warp.shape[1]/6)]
+
+        cropped = cv.GaussianBlur(cropped, (5, 5), 0)
+
+        croppedVal = warp[0:50, 0:40]
+        croppedType = warp[50:100, 0:40]
+
+        cv.imshow("Cropped", cropped)
+        cv.imshow("Val", croppedVal)
+        cv.imshow("Type", croppedType)
 
         method = cv.TM_CCOEFF_NORMED
-        result = cv.matchTemplate(warp, template, method)
-        cv.normalize(result, result, 0, 1, cv.NORM_MINMAX, -1)
-        _minVal, _maxVal, minLoc, maxLoc = cv.minMaxLoc(result, None)
-        if (method == cv.TM_SQDIFF or method == cv.TM_SQDIFF_NORMED):
-            matchLoc = minLoc
-        else:
-            matchLoc = maxLoc
+    
+        resultType = cv.matchTemplate(croppedType, templateType, method)
+        resultVal = cv.matchTemplate(croppedVal, templateVal, method)
 
-        cv.rectangle(frame, matchLoc, (matchLoc[0] + template.shape[0],
-                     matchLoc[1] + template.shape[1]), (0, 0, 0), 2, 8, 0)
-        cv.rectangle(result, matchLoc, (matchLoc[0] + template.shape[0],
-                     matchLoc[1] + template.shape[1]), (0, 0, 0), 2, 8, 0)
+        # cv.normalize(result, result, 0, 1, cv.NORM_MINMAX, -1)
+        _minVal, _maxVal, minLoc, maxLoc = cv.minMaxLoc(resultType)
+        if(_maxVal < 0.6):
+            print("Not Match Type: " + str(_maxVal))
+            cv.waitKey(1000)
+            continue
 
-                    
+        _minVal, _maxVal, minLoc, maxLoc = cv.minMaxLoc(resultVal)
+        if(_maxVal < 0.6):
+            print("Not Match Val " + str(_maxVal))
+            cv.waitKey(1000)
+            continue
+        
+        print("Match " + str(_maxVal))
+
+        # if (method == cv.TM_SQDIFF or method == cv.TM_SQDIFF_NORMED):
+        #     matchLoc = minLoc
+        # else:
+        #     matchLoc = maxLoc
+        
+        # cv.rectangle(template, matchLoc, (matchLoc[0] + template.shape[0],
+        #             matchLoc[1] + template.shape[1]), 255, 2)
+
+        cv.imshow("Template", template)            
         cv.imshow("Original", frame)
-        cv.imshow("Matching", result)
+        cv.imshow("Matching", resultType)
 
-    cv.waitKey(50)
+    cv.waitKey(1000)
