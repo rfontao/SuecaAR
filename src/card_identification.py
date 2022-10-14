2c378de57744b3ea7e86275dd557424ac33ed275
rfontao <ricardo.fontao@gmail.com>
2022-10-15 00:12:39 +0100

    Rank and suit identification

---
+++ src/card_identification.py
@@ -0,0 +1,93 @@
+import cv2 as cv
+
+
+class CardIdentifier():
+
+    suit_paths = {
+        "Spades": "1.png",
+        "Hearts": "2.png",
+        "Clubs": "3.png",
+        "Diamonds": "4.png",
+    }
+
+    rank_paths = {
+        "A": "1.png",    # A
+        "2": "5.png",    # 2
+        "3": "9.png",    # 3
+        "4": "13.png",   # 4
+        "5": "17.png",   # 5
+        "6": "21.png",   # 6
+        "7": "25.png",   # 7
+        "Q": "41.png",   # Q
+        "J": "45.png",   # J
+        "K": "49.png",   # K
+    }
+
+    def __init__(self):
+        self.load_suit_database()
+        self.load_rank_database()
+
+    def load_suit_database(self):
+        self.suit_database = []
+        for k, v in self.suit_paths.items():
+            img = cv.imread(f"../cards/full/{v}", cv.IMREAD_GRAYSCALE)
+            img = img[90:180, 10:90]  # Extracting the rank of the card
+            _, img = cv.threshold(img, 225, 255, cv.THRESH_BINARY)
+            self.suit_database.append((k, img))
+
+    def load_rank_database(self):
+        self.rank_database = []
+        for k, v in self.rank_paths.items():
+            img = cv.imread(f"../cards/full/{v}", cv.IMREAD_GRAYSCALE)
+            img = img[15:85, 25:75]  # Extracting the suit of the card
+            _, img = cv.threshold(img, 225, 255, cv.THRESH_BINARY)
+            self.rank_database.append((k, img))
+
+    @classmethod
+    def process_image_to_match(cls, image):
+        image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+        image = cv.GaussianBlur(image, (5, 5), 0)
+        _, image = cv.threshold(image, 225, 255, cv.THRESH_BINARY)
+
+        # Test later with erosions (from: https://repositorio-aberto.up.pt/bitstream/10216/59981/1/000146528.pdf)
+        # kernel = np.ones((3, 3), np.uint8)
+        # templateVal = cv.erode(templateVal, kernel, iterations=1)
+        return image
+
+    @classmethod
+    def match_template(cls, image, template):
+        """Image should be previously processed with process_image_to_match"""
+        contours, _ = cv.findContours(
+            image, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        contours = sorted(contours, key=cv.contourArea, reverse=True)
+        if len(contours) > 1:
+            x, y, w, h = cv.boundingRect(contours[1])
+            image = image[y:y+h, x:x+w]
+
+        image = cv.resize(
+            image, (template.shape[1], template.shape[0]), interpolation=cv.INTER_LINEAR)
+
+        result = cv.matchTemplate(image, template, cv.TM_SQDIFF_NORMED)
+        _minVal, _maxVal, minLoc, maxLoc = cv.minMaxLoc(result)
+        return _minVal
+
+    def identify_rank(self, rank_image):
+        scores = []
+        rank_image = CardIdentifier.process_image_to_match(rank_image)
+        for rank, template in self.rank_database:
+            # cv.imshow("RANK", template)
+            scores.append((rank, CardIdentifier.match_template(
+                rank_image, template.copy())))
+
+        scores = sorted(scores, key=lambda x: x[1])
+        return scores
+
+    def identify_suit(self, suit_image):
+        scores = []
+        suit_image = CardIdentifier.process_image_to_match(suit_image)
+        for suit, template in self.suit_database:
+            scores.append((suit, CardIdentifier.match_template(
+                suit_image, template.copy())))
+
+        scores = sorted(scores, key=lambda x: x[1])
+        return scores
--- src/detect_card.py
+++ src/detect_card.py
@@ -1,5 +1,4 @@
-from copyreg import constructor
-from email.mime import image
+from card_identification import CardIdentifier
 import cv2 as cv
 import numpy as np
 import sys
@@ -14,16 +13,7 @@ if (len(sys.argv) < 2):
 # Capture video from webcam
 camera = Camera(sys.argv[2], sys.argv[1])
 detector = CardDetector(True)
-
-cardimg = cv.imread("../cards/full/5.png", cv.IMREAD_COLOR)
-cardVal = cardimg[15:85, 25:75]  # gets the value of the card
-cardVal = cv.cvtColor(cardVal, cv.COLOR_BGR2GRAY)
-_, cardVal = cv.threshold(cardVal, 225, 255, cv.THRESH_BINARY)
-
-cardType = cardimg[90:200, 10:90]  # gets the type of the card
-
-cv.imshow("CardVal", cardVal)
-cv.imshow("CardType", cardType)
+identifier = CardIdentifier()
 
 while True:
     frame = camera.get_frame()
@@ -35,24 +25,23 @@ while True:
             [500, 0],
             [500, 726],
             [0, 726]
-            ], dtype="float32")
+        ], dtype="float32")
 
         sortedPoints, center = detector.sortCardPoints(cards[0])
-        print(sortedPoints)
-        print("center", center)
+        # print(sortedPoints)
+        # print("center", center)
         x, y = cards[0][0][0]
         x1, y1 = sortedPoints[0][0]
         x2, y2 = sortedPoints[1][0]
         x3, y3 = sortedPoints[2][0]
         x4, y4 = sortedPoints[3][0]
 
-        cv.circle(frame, [int(x), int(y)], 2, (0,0,0), 5)
-        cv.circle(frame, [int(x1), int(y1)], 2, (255,0,0), 2)
-        cv.circle(frame, [int(x2), int(y2)], 2, (0,255,0), 2)
-        cv.circle(frame, [int(x3), int(y3)], 2, (0,0,255), 2)
-        cv.circle(frame, [int(x4), int(y4)], 2, (255,255,255), 2)
-        cv.circle(frame, [int(center[0]), int(center[1])], 2, (255,0,255), 2)
-
+        # cv.circle(frame, [int(x), int(y)], 2, (0, 0, 0), 5)
+        # cv.circle(frame, [int(x1), int(y1)], 2, (255, 0, 0), 2)
+        # cv.circle(frame, [int(x2), int(y2)], 2, (0, 255, 0), 2)
+        # cv.circle(frame, [int(x3), int(y3)], 2, (0, 0, 255), 2)
+        # cv.circle(frame, [int(x4), int(y4)], 2, (255, 255, 255), 2)
+        # cv.circle(frame, [int(center[0]), int(center[1])], 2, (255, 0, 255), 2)
 
         M = cv.getPerspectiveTransform(sortedPoints, dst)
         warp = cv.warpPerspective(frame, M, (500, 726))
@@ -61,87 +50,17 @@ while True:
 
         # cv.imshow("Warped", warp)
         template = warp[0:int(warp.shape[0]/4), 0:int(warp.shape[1]/6)]
-
         template = cv.GaussianBlur(template, (5, 5), 0)
+        templateVal = warp[0:90, 0:90]
+        templateType = warp[80:200, 0:90]
 
-        templateVal = warp[0:90, 0:80]
-        templateType = warp[90:200, 0:80]
+        cv.imshow("suit", templateType)
+        cv.imshow("rank", templateVal)
 
-        t1 = templateVal.copy()
-        templateVal = cv.cvtColor(templateVal, cv.COLOR_BGR2GRAY)
-        templateVal = cv.GaussianBlur(templateVal, (5, 5), 0)
-        _, templateVal = cv.threshold(templateVal, 225, 255, cv.THRESH_BINARY)
+        print(identifier.identify_rank(templateVal))
+        print(identifier.identify_suit(templateType))
 
-        # Test later with erosions (from: https://repositorio-aberto.up.pt/bitstream/10216/59981/1/000146528.pdf)
-        # kernel = np.ones((3, 3), np.uint8)
-        # templateVal = cv.erode(templateVal, kernel, iterations=1)
-
-        # templateVal = cv.bitwise_not(templateVal)
-        contours, _ = cv.findContours(
-            templateVal, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-        contours = sorted(contours, key=cv.contourArea, reverse=True)
-        if len(contours) > 1:
-            rect = cv.boundingRect(contours[1])
-            # t1 = cv.rectangle(t1, (int(rect[0]), int(rect[1])), (int(
-            #     rect[0] + rect[2]), int(rect[1] + rect[3])), (0, 0, 255), 3)
-
-            # print(len(contours))
-            # t1 = cv.drawContours(t1, contours, -1, (0, 255, 0), 2)
-
-            templateVal = templateVal[rect[1]:rect[1]+rect[3], rect[0]:rect[0]+rect[2]]
-
-        templateVal = cv.resize(
-            templateVal, (cardVal.shape[1], cardVal.shape[0]), interpolation=cv.INTER_LINEAR)
-
-        # templateVal = cv.cvtColor(templateVal, cv.COLOR_BGR2GRAY)
-        # _, templateVal = cv.threshold(templateVal, 200, 255, cv.THRESH_BINARY)
-        # coords = cv.findNonZero(templateVal)
-        # x, y, w, h = cv.boundingRect(coords)
-        # templateVal = templateVal[y:y+h, x:x+w]
-
-        cv.imshow("template", template)
-        cv.imshow("Val", templateVal)
-        cv.imshow("Type", templateType)
-
-        # method = cv.TM_CCOEFF_NORMED
-        method = cv.TM_SQDIFF_NORMED
-
-        # resultType = cv.matchTemplate(template, cardimg, method)
-        resultType = cv.matchTemplate(templateVal, cardVal, method)
-
-        # cv.normalize(result, result, 0, 1, cv.NORM_MINMAX, -1)
-        # cv.imshow("Template", template)            
         cv.imshow("Original", frame)
-        # cv.imshow("Matching", resultType)
-        # cv.imshow("Match", copy)
-
-        _minVal, _maxVal, minLoc, maxLoc = cv.minMaxLoc(resultType)
-        if(_minVal > 0.45):
-            print("Not Match Type: " + str(_minVal))
-            cv.waitKey(100)
-            continue
-
-        # _minVal, _maxVal, minLoc, maxLoc = cv.minMaxLoc(resultVal)
-        # if(_maxVal < 0.8):
-        #     print("Not Match Val " + str(_maxVal))
-        #     cv.waitKey(1000)
-        #     continue
-
-        print("Match " + str(_maxVal))
-
-        if (method == cv.TM_SQDIFF or method == cv.TM_SQDIFF_NORMED):
-            matchLoc = minLoc
-        else:
-            matchLoc = maxLoc
-
-        copy = cardimg.copy()
-
-        cv.rectangle(copy, matchLoc, (matchLoc[0] + template.shape[0],
-                                      matchLoc[1] + copy.shape[1]), 255, 2)
-
         cv.imshow("Template", template)
-        cv.imshow("Original", frame)
-        cv.imshow("Matching", resultType)
-        cv.imshow("Match", copy)
 
-    cv.waitKey(50)
+    cv.waitKey(500)
